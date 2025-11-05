87b16b8265aede34e99085d36674c0e967dd54df
johan626 <j6749520@gmail.com>
2025-11-05 10:39:15 +0700

    Create SelectATool.model.lua

---
+++ UI/SelectATool.model.lua
@@ -0,0 +1,16 @@
+return {
+	ClassName = "TextLabel",
+
+	Properties = {
+		BackgroundColor3 = Color3.new(0.125, 0.125, 0.125),
+		BackgroundTransparency = 0.1,
+
+		TextColor3 = Color3.new(1, 1, 1),
+		Size = UDim2.new(1, 0, 1, 0),
+
+		Font = Enum.Font.SourceSansSemibold,
+		Text = "Select a Tool\n",
+
+		TextSize = 36,
+	},
+}
